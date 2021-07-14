f4e6042b54dda0fd545e385b1e33f2f5196dfd0c
HugoEmannuel <hugoemannuel53@gmail.com>
2021-07-14 16:36:59 -0300

    bloco 5

---
+++ bloco_4/dia4/scirpt.js
@@ -0,0 +1,33 @@
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
+
+// console.log('Bem vinda', info['personagem']);
+
+// ex2
+
+//   info['recorrente'] = 'sim';
+//   console.log(info);
+
+// ex3
+
+//   for (let i in info) {
+//     console.log(i)
+//   }
+
+//    ex4
+//   for (let i in info) {
+//     console.log(info[i])
+//   }
+
+//   ex5
+
+//   let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'sim'
+//   }
+//   console.log(info['personagem'] , info2['personagem'])
